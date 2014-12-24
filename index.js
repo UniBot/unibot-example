30dd40335c0c42495a1763b32474c262b3ecefc6
Dean Sofer <dean@Deans-MacBook-Pro.local>
2014-12-23 17:02:03 -0800

    Refactored plugin for new structure

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-// If you want a DB connection
-// var mongoose = require('mongoose');
+/**
+ * Commands Plugin for Unibot
+ * @param  {Object} options [description]
+ *   db: {mongoose} the mongodb connection
+ *   bot: {irc} the irc bot
+ *   web: {connect} a connect + connect-rest webserver
+ *   config: {object}
+ * @return {Function}         init function to access shared resources
+ */
+module.exports = function init(options){
 
-// Executed once per bot
+  var mongoose = options.db;
+  var bot = options.bot;
+  var webserver = options.web;
+  var config = options.config;
 
-var plugin = function(channel, config){
-  // Executed once per channel
-  
-  // channel.join(function(who){ /* do something */ });
-  // channel.leave(function(who, reason){ /* do something */ });
-  // regexpattern, callback
-  channel.message('^hi unibot', function(from, matches){
-    channel.say('Hello ' + from); // in-channel reply
-    channel.say('Hello', from); // private reply
-  });
-};
-
-plugin.load = function(app) {
-  app.get('/example', function(req, res, next){
+  webserver.get('/example', function(req, res, next){
     res.sendFile('example.html');
   });
-  app.get('/example/:channel', function(req, res, next) {
+
+  webserver.get('/example/:channel', function(req, res, next) {
     // mongoose.model(...).findOne({ channel: req.params.channel }, function(err, example){
       res.send({ example: 'Hello World!'});
     // });
   });
-}
+
+  return function plugin(channel){
+    // Executed once per channel
+    
+    // channel.join(function(who){ /* do something */ });
+    // channel.leave(function(who, reason){ /* do something */ });
+    // regexpattern, callback
+    channel.message('^hi unibot', function(from, matches){
+      channel.say('Hello ' + from); // in-channel reply
+      channel.say('Hello', from); // private reply
+    });
+  };
  
-module.exports = plugin;
+};
